68d83e84004d113f8be20e62a963d0855e0ccee3
Yerassil <yerassil@gmail.com>
2020-02-13 00:22:25 +0600

    Add base templates, add index view for articles list

--- django_articles/settings.py
+++ django_articles/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'django_articles.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(PROJECT_ROOT, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
