223a4b0f5626335b86c758b0a6257830089b5c09
Yerassil <yerassil@gmail.com>
2020-02-17 22:47:21 +0600

    Fix linter errors

--- django_articles/apps/articles/views.py
+++ django_articles/apps/articles/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
-from .models import Article, Comment
+from .models import Article
+
 
 def index(request):
     latest_articles_list = Article.objects.order_by('-pub_date')[:5]
-    return render(request, 'articles/list.html', {'latest_articles_list': latest_articles_list})
+    return render(
+        request, 'articles/list.html',
+        {'latest_articles_list': latest_articles_list})
